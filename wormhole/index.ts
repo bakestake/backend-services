18bdd61d6e64d44bb45d6524014501b16ba406f0
Rushikesh0125 <jadhav.rushikesh.ggl@gmail.com>
2024-07-18 12:36:06 +0530

    "header update"

--- wormhole/index.ts
+++ wormhole/index.ts
@@ -17,7 +17,7 @@ app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, API-KEY')
+  res.header('Access-Control-Allow-Headers', 'API-KEY')
   next()
 })
 

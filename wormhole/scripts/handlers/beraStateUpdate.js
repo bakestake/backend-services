a1c609ded3da2ae534e10e018d572391f4bcb804
Rushikesh0125 <jadhav.rushikesh.ggl@gmail.com>
2024-08-05 18:00:51 +0530

    "corrections"

--- wormhole/scripts/handlers/beraStateUpdate.js
+++ wormhole/scripts/handlers/beraStateUpdate.js
@@ -21,54 +21,49 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config({ path: "../../.env" });
 const BeraStateUpdate = () => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b, _c, _d, _e, _f, _g, _h;
-    try {
-        console.log("updating bera");
-        const chains = [
-            { chains: "fuji", chainId: 6, rpc: (0, getProviderUrl_1.getProviderURLs)("fuji") },
-            { chains: "arbSepolia", chainId: 10003, rpc: (0, getProviderUrl_1.getProviderURLs)("arbSepolia") },
-            { chains: "amoy", chainId: 10007, rpc: (0, getProviderUrl_1.getProviderURLs)("amoy") },
-            { chains: "bscTestnet", chainId: 4, rpc: (0, getProviderUrl_1.getProviderURLs)("bscTestnet") },
-            { chains: "beraTestnet", chainId: 39, rpc: (0, getProviderUrl_1.getProviderURLs)("beraTestnet") },
-            // { chains: "coreTestnet", chainId: 4, rpc: getProviderURLs("coreTestnet") },
-            { chains: "baseSepolia", chainId: 10004, rpc: (0, getProviderUrl_1.getProviderURLs)("baseSepolia") },
-        ];
-        console.log("getting responses from all chains");
-        const responses = yield Promise.all(chains.map(({ rpc, chainId }) => rpc
-            ? axios_1.default
-                .post(rpc, [
-                {
-                    jsonrpc: "2.0",
-                    id: 1,
-                    method: "eth_getBlockByNumber",
-                    params: ["latest", false],
-                },
-                {
-                    jsonrpc: "2.0",
-                    id: 2,
-                    method: "eth_call",
-                    params: [{ to: "0x26705aD938791e61Aa64a2a9D808378805aec819", data: "0x4269e94c" }, "latest"],
-                },
-            ])
-                .catch((error) => {
-                console.error(`Error fetching data for rpc: ${rpc}`, error);
-                return null;
-            })
-            : Promise.reject(new Error(`RPC URL is undefined for chain ${chainId}`))));
-        console.log("summing up global");
-        let globalBudsCount = 0;
-        for (let i = 0; i < responses.length; i++) {
-            console.log(parseInt((_d = (_c = (_b = (_a = responses[i]) === null || _a === void 0 ? void 0 : _a.data) === null || _b === void 0 ? void 0 : _b[0]) === null || _c === void 0 ? void 0 : _c.result) === null || _d === void 0 ? void 0 : _d.number));
-            globalBudsCount += parseInt((_h = (_g = (_f = (_e = responses[i]) === null || _e === void 0 ? void 0 : _e.data) === null || _f === void 0 ? void 0 : _f[0]) === null || _g === void 0 ? void 0 : _g.result) === null || _h === void 0 ? void 0 : _h.number);
-        }
-        console.log(globalBudsCount);
-        console.log("updating bera state");
-        const setterInst = new ethers_1.ethers.Contract("0x26705ad938791e61aa64a2a9d808378805aec819", setter_1.ABI, new ethers_1.ethers.Wallet(process.env.STATE_PRIVATE_KEY || "", new ethers_1.ethers.JsonRpcProvider((0, getProviderUrl_1.getProviderURLs)("beraTestnet") || "")));
-        console.log("doing tx");
-        const tx = yield setterInst.setGlobalBudsState(globalBudsCount);
-        yield tx.wait();
+    console.log("updating bera");
+    const chains = [
+        { chains: "fuji", chainId: 6, rpc: (0, getProviderUrl_1.getProviderURLs)("fuji") },
+        { chains: "arbSepolia", chainId: 10003, rpc: (0, getProviderUrl_1.getProviderURLs)("arbSepolia") },
+        { chains: "amoy", chainId: 10007, rpc: (0, getProviderUrl_1.getProviderURLs)("amoy") },
+        { chains: "bscTestnet", chainId: 4, rpc: (0, getProviderUrl_1.getProviderURLs)("bscTestnet") },
+        { chains: "beraTestnet", chainId: 39, rpc: (0, getProviderUrl_1.getProviderURLs)("beraTestnet") },
+        // { chains: "coreTestnet", chainId: 4, rpc: getProviderURLs("coreTestnet") },
+        { chains: "baseSepolia", chainId: 10004, rpc: (0, getProviderUrl_1.getProviderURLs)("baseSepolia") },
+    ];
+    console.log("getting responses from all chains");
+    const responses = yield Promise.all(chains.map(({ rpc, chainId }) => rpc
+        ? axios_1.default
+            .post(rpc, [
+            {
+                jsonrpc: "2.0",
+                id: 1,
+                method: "eth_getBlockByNumber",
+                params: ["latest", false],
+            },
+            {
+                jsonrpc: "2.0",
+                id: 2,
+                method: "eth_call",
+                params: [{ to: "0x26705aD938791e61Aa64a2a9D808378805aec819", data: "0x4269e94c" }, "latest"],
+            },
+        ])
+            .catch((error) => {
+            console.error(`Error fetching data for rpc: ${rpc}`, error);
+            return null;
+        })
+        : Promise.reject(new Error(`RPC URL is undefined for chain ${chainId}`))));
+    console.log("summing up global");
+    let globalBudsCount = 0;
+    for (let i = 0; i < responses.length; i++) {
+        console.log(parseInt((_d = (_c = (_b = (_a = responses[i]) === null || _a === void 0 ? void 0 : _a.data) === null || _b === void 0 ? void 0 : _b[0]) === null || _c === void 0 ? void 0 : _c.result) === null || _d === void 0 ? void 0 : _d.number));
+        globalBudsCount += parseInt((_h = (_g = (_f = (_e = responses[i]) === null || _e === void 0 ? void 0 : _e.data) === null || _f === void 0 ? void 0 : _f[0]) === null || _g === void 0 ? void 0 : _g.result) === null || _h === void 0 ? void 0 : _h.number);
     }
-    catch (error) {
-        console.log(error);
-    }
+    console.log(globalBudsCount);
+    console.log("updating bera state");
+    const setterInst = new ethers_1.ethers.Contract("0x054e0D70FfaefE4d7329B5077B4EcdE8062bfc08", setter_1.ABI, new ethers_1.ethers.Wallet(process.env.STATE_PRIVATE_KEY || "", new ethers_1.ethers.JsonRpcProvider((0, getProviderUrl_1.getProviderURLs)("beraTestnet") || "")));
+    console.log("doing tx");
+    const tx = yield setterInst.setGlobalStakedBuds(globalBudsCount);
+    yield tx.wait();
 });
 exports.BeraStateUpdate = BeraStateUpdate;
